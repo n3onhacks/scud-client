0bd731a2322ab3928e9e62b0a127ecc5dd3f72e5
n3on <97998625+n3onhacks@users.noreply.github.com>
2022-10-18 01:54:14 -0400

    Delete first.sh

--- chat/first.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-servuIp=$(sed -n '1p' group)
-dracula=$(sed -n '2p' group)
-logLoc=$(sed -n '3p' group)
-echo "Welcome to the chat" > chat.txt
-sshpass -p $dracula scp $servuIp:$logLoc/outgoing/chat.txt .
-cat chat.txt
